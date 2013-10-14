5d5d79f30aa8fae77f08cbe30b4843270ab96efa
maieul <maieul@maieul.net>
2013-10-14 17:49:53 +0200

    cas des pages en chiffres romains

--- catalogues.py
+++ catalogues.py
@@ -1,16 +1,20 @@
 #les paramètres
-base="http://xeir.nlg.gr/images/9/"
+base="http://xeir.nlg.gr/images/15/"
 min = 1
-max = 27
-
+max = 39
+romain = True
 i = min
 import urllib.request
+import roman
 while i <= max:
     try:   
-        url = base + str(i).zfill(3)+".gif"
+        if romain:
+            url = base + roman.toRoman(i).lower()+".gif"
+        else:
+            url = base + str(i).zfill(3)+".gif"
         print (url)
         http = urllib.request.urlopen(url)
-        file = open(str(i).zfill(3)+".gif","wb")
+        file = open("435/"+str(i).zfill(3)+".gif","wb")
         file.write(http.read())
         file.close()
     except:
