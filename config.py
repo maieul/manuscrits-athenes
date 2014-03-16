1372d051b436b070077086f0e3da6f4ecd5aad1b
maieul <maieul@maieul.net>
2014-03-16 12:16:36 +0100

    mettre les fichiers de config à part

---
+++ config.py
@@ -0,0 +1,9 @@
+#les paramètres
+base="http://babel.hathitrust.org/cgi/imgsrv/image?id=mdp.39015012425404;seq={nombre}"
+extension = ".jpg"
+min = 1
+max = 2
+romain = False
+
+dossier="Schlumberger1884"
+complete = 0
