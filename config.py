7070638df4d389732decb8a4dd6185c17f69863c
maieul <maieul@maieul.net>
2014-05-08 16:12:04 +0200

    réorganiser ordre des paramètres

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 #les paramètres
+## les + importants
 base="http://babel.hathitrust.org/cgi/imgsrv/image?id=mdp.39015012425404;seq={nombre}"
 extension = ".jpg"
 min = 1
 max = 2
-romain = False
-
 dossier="Schlumberger1884"
+
+## les secondaires
+romain = False
 complete = 0
